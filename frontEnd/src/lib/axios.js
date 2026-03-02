ce8aca275d1f0ba3a79f6f4a3e1f9f66f83fabef
unknown <fazeelimam6@gmail.com>
2026-03-03 01:58:14 +0500

    Add axios interceptor to handle 401 silently

--- frontEnd/src/lib/axios.js
+++ frontEnd/src/lib/axios.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
@@ -8,4 +8,18 @@ export const axiosInstance = axios.create({
     headers: {
         "Content-Type": "application/json",
     }
-})
+});
+
+// Add response interceptor to handle 401 errors silently
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        // Don't show errors for 401 on /getuser (user just not logged in)
+        if (error.config.url === '/getuser' && error.response?.status === 401) {
+            // Silently fail - this is expected when not logged in
+            return Promise.reject(error);
+        }
+
+        return Promise.reject(error);
+    }
+);
